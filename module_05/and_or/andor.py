4df1313fb43449e7efb2f68b9ea0c89c41fce688
rt75272 <rt75272@gmail.com>
2024-01-09 16:18:53 -0700

    Init Module 5

---
+++ module_05/and_or/andor.py
@@ -0,0 +1,5 @@
+user_input = int(input("What is the answer to the universe?"))
+answer = 42
+
+if user_input == answer and True:
+    print("Correct")
---
+++ module_05/ctr/rain.py
@@ -0,0 +1,23 @@
+num_years = int(input("Enter the number of years: "))
+
+years = [] # TODO: Add rain info dictionaries as a single entry for each year. 
+
+rain_info = {
+    "January" : 0,
+    "Febuary" : 0,
+    "March" : 0,
+    "April" : 0,
+    "May" : 0,
+    "June" : 0,
+    "July" : 0,
+    "August" : 0,
+    "September" : 0,
+    "October" : 0,
+    "November" : 0,
+    "December" : 0
+}
+
+for i in range(num_years):
+    for month in rain_info:
+        print(rain_info[month])
+    years.append(rain_info) # Adds entire year of rain info as one entry each.
