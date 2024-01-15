8034a06399bc647db30a1c9d196645fb225b70f5
rt75272 <rt75272@gmail.com>
2024-01-15 16:33:46 -0700

    Added some simple AND/OR examples

--- module_05/and_or/andor.py
+++ module_05/and_or/andor.py
@@ -1,5 +1,23 @@
-user_input = int(input("What is the answer to the universe?"))
-answer = 42
+# Ask if the user knows the answer to the universe.
+user_input = int(input("What is the answer to the universe? ")) 
+answer = 42 # Set the answer to the universe.
 
+# Simple AND example.
 if user_input == answer and True:
     print("Correct")
+else:
+    print("Incorrect")
+
+# Simple OR example.
+if user_input == answer or False:
+    print("Correct")
+else:
+    print("Incorrect")
+
+# Simple AND example.
+if True and True:
+    print("True")
+
+# Simple OR example.
+if True or False:
+    print("True")
