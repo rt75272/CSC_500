11114b5cb5a93188dbafa9867a2acb6c453226c4
rt75272 <rt75272@gmail.com>
2024-01-16 16:02:48 -0700

    Module 5 Discussion Complete

--- module_05/and_or/andor.py
+++ module_05/and_or/andor.py
@@ -2,14 +2,17 @@
 user_input = int(input("What is the answer to the universe? ")) 
 answer = 42 # Set the answer to the universe.
 
-# Simple AND example.
-if user_input == answer and True:
+x = 0
+y = 1
+
+# AND example.
+if user_input == answer and y == 1:
     print("Correct")
 else:
     print("Incorrect")
 
-# Simple OR example.
-if user_input == answer or False:
+# OR example.
+if user_input == answer or x == 1:
     print("Correct")
 else:
     print("Incorrect")
@@ -21,3 +24,11 @@ if True and True:
 # Simple OR example.
 if True or False:
     print("True")
+
+
+bomb = False
+safe = True
+if(safe and not bomb):
+    print("False alarm.")
+else:
+    print("Boom.")
