734b037211c1b836ccb5d7c3a0897453747f7502
rt75272 <rt75272@gmail.com>
2024-01-18 12:40:06 -0700

    Init functions
        - Addded placeholder functions to be expanded upon.

--- module_05/ctr/rain.py
+++ module_05/ctr/rain.py
@@ -1,6 +1,6 @@
 num_years = int(input("Enter the number of years: "))
 
-years = [] # TODO: Add rain info dictionaries as a single entry for each year. 
+years = []
 
 rain_info = {
     "January" : 0,
@@ -22,3 +22,21 @@ for i in range(num_years):
         rain_info[month] = int(input(f"Enter the rainfall amount for {month}: "))
         print(rain_info[month])
     years.append(rain_info) # Adds entire year of rain info as one entry each.
+
+print(years)
+
+
+# Total number of months recorded. 
+def num_months():
+    return 0
+
+# Total rainfall for all months recorded.
+def total_rainfall():
+    return 0
+
+# Average rainfall over the course of all recorded months.
+def avg_rainfall():
+    return 0
+
+def main():
+    return 0
