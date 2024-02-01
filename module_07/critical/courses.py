3d42cf1e1dfa35d6981d3390df670891cad4a49a
rt75272 <rt75272@gmail.com>
2024-02-01 12:39:44 -0700

    Init Module 7 Critical Thinking Assignment
        - Module 7 discussion complete.

---
+++ module_07/critical/courses.py
@@ -0,0 +1,25 @@
+room_number = {
+    "CSC101" : 3004,
+    "CSC102" : 4501,
+    "CSC103" : 6755,
+    "NET110" : 1244,
+    "COM241" : 1411
+}
+
+instructor = {
+    "CSC101" : "Haynes",
+    "CSC102" : "Alvarado",
+    "CSC103" : "Rich",
+    "NET110" : "Burke",
+    "COM241" : "Lee"
+}
+
+time = {
+    "CSC101" : "8:00 a.m.",
+    "CSC102" : "9:00 a.m.",
+    "CSC103" : "10:00 a.m.",
+    "NET110" : "11:00 a.m.",
+    "COM241" : "1:00 p.m."
+}
+
+course = input("Which course would you like to look up?")
