e745cf03f0a5ff37e1155fecc6a534b5d282e0c3
rt75272 <rt75272@gmail.com>
2024-02-04 16:37:33 -0700

    Module 7 Complete

--- module_07/critical/courses.py
+++ module_07/critical/courses.py
@@ -1,4 +1,18 @@
-room_number = {
+import re
+from termcolor import colored
+# ----------------------------------------------------------------------------- 
+# Course Look Up
+#
+# Allows the user to enter their desired course number and displays all the 
+# available information on the course.
+#
+# Usage:
+#   $ python courses.py
+#   [follow prompt to complete program]
+# -----------------------------------------------------------------------------
+
+# Dictionary of room numbers
+room_numbers = {
     "CSC101" : 3004,
     "CSC102" : 4501,
     "CSC103" : 6755,
@@ -6,7 +20,8 @@ room_number = {
     "COM241" : 1411
 }
 
-instructor = {
+# Dictionary of instructors 
+instructors = {
     "CSC101" : "Haynes",
     "CSC102" : "Alvarado",
     "CSC103" : "Rich",
@@ -14,7 +29,8 @@ instructor = {
     "COM241" : "Lee"
 }
 
-time = {
+# Dictionary of times.
+times = {
     "CSC101" : "8:00 a.m.",
     "CSC102" : "9:00 a.m.",
     "CSC103" : "10:00 a.m.",
@@ -22,4 +38,43 @@ time = {
     "COM241" : "1:00 p.m."
 }
 
-course = input("Which course would you like to look up?")
+# ----------------------------------------------------------------------
+# Get Course Information
+#
+# Gets all the course values and displays in the terminal for the user. 
+# Returns none.
+# ----------------------------------------------------------------------
+def get_course_info(course):
+    room_number = room_numbers[course]
+    instructor = instructors[course]
+    time = times[course]
+    print(
+        colored(
+            f"\n{course} is in room {room_number} with {instructor} at {time}\n", 
+            "cyan", attrs=['bold']
+        )
+    )
+
+# Grab the desired course number from the user.
+course = input("\nWhich course would you like to look up?\n")
+# Regex to locate CSC101-CSC103.
+csc_course = re.findall("CSC10[1-3]", course)
+# Grabbing the length of x incase the course in not found.
+n = len(csc_course)
+
+# Check n(the length of a found csc course) and then check the course number. 
+if n > 0 and course == csc_course[0]:
+    get_course_info(course)
+# Check for the course NET110.
+elif course == "NET110":
+    get_course_info(course)
+# Check for the course COM241.
+elif course == "COM241":
+    get_course_info(course)
+# Else for no course found.
+else:
+    print(
+        colored(
+            f"\nThe course {course} was not found\n", "red", attrs=['bold']
+        )
+    )
